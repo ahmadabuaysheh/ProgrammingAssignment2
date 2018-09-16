0691e1c1bc6783498f03cdca9365b81c9a1ca3df
Ahmad Abu-Aysheh <a.abuaysheh@cyphergard.com>
2018-09-16 23:47:57 +0300

    Finish assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+library(matlib)
+pmakeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setmean = setmean,getmean=getmean)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- inv(data, ...)
+    x$setinverse(m)
+    m
 }
